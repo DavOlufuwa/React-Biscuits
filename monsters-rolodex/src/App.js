9e8cfca9ef7a78892f3785bf429d531a6b5202fc
David Olufuwa <david.olufuwa@thebulbafrica.institute>
2022-05-25 12:48:40 +0100

    Learnt about basic components, state and setState() method

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,46 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+// turning the original function into a Class that extends from the super Class "Component"
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(){
+    super();
+
+    this.state = {
+      monsters :[]
+ 
+      
+
+    };
+  }
+  // fetching data from the API returns a promise
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users)=> this.setState(()=>{
+      return{
+        monsters: users
+      }
+    },()=>{
+      console.log(this.state);
+    }))
+  }
+
+
+  // this is what we are rendering we use the render method.
+  render(){
+    return (
+      <div className="App">
+        <h1>
+         {
+           this.state.monsters.map((monster)=>{
+              return<div key={monster.id}>{monster.name}</div>
+           })
+         }
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
